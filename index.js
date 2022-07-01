5f61de64ee356aa638bb7ff255155d448d2313e4
Linh Vu <linh777gkog@gmail.com>
2022-07-01 22:47:59 +0700

    push code

--- index.js
+++ index.js
@@ -113,4 +113,10 @@ window.addEventListener('DOMContentLoaded', () => {
       tile.classList.remove('playerO');
     });
   };
+
+  tiles.forEach((tile, index) => {
+    tile.addEventListener('click', () => userAction(tile, index));
+  });
+
+  resetButton.addEventListener('click', resetBoard);
 });
